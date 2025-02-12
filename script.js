c4e35da7ece71740cd5b8c155a816ab43c134d90
J4cKr0y <Kiky270281@gmail.com>
2025-02-12 22:30:11 +0100

    Ajout de chekOrientation. C'est injouable en mode portrait lol

--- script.js
+++ script.js
@@ -1,127 +1,108 @@
+function checkOrientation() {
+    if (window.innerHeight > window.innerWidth) {
+        alert('Veuillez mettre votre appareil en mode horizontal pour une meilleure expérience de jeu.');
+    }
+}
+
+window.addEventListener('resize', checkOrientation);
+window.addEventListener('orientationchange', checkOrientation);
+
+checkOrientation();
+
 let gameState = 'start';
-        let paddle_1 = document.querySelector('.paddle_1');
-        let paddle_2 = document.querySelector('.paddle_2');
-        let board = document.querySelector('.board');
-        let initial_ball = document.querySelector('.ball');
-        let ball = document.querySelector('.ball');
-        let score_1 = document.querySelector('.player_1_score');
-        let score_2 = document.querySelector('.player_2_score');
-        let message = document.querySelector('.message');
-        let paddle_1_coord = paddle_1.getBoundingClientRect();
-        let paddle_2_coord = paddle_2.getBoundingClientRect();
-        let initial_ball_coord = ball.getBoundingClientRect();
-        let ball_coord = initial_ball_coord;
-        let board_coord = board.getBoundingClientRect();
-        let paddle_common = 
-            document.querySelector('.paddle').getBoundingClientRect();
-        let dx = Math.floor(Math.random() * 4) + 3;
-        let dy = Math.floor(Math.random() * 4) + 3;
-        let dxd = Math.floor(Math.random() * 2);
-        let dyd = Math.floor(Math.random() * 2);
-        
-        document.addEventListener('keydown', (e) => {
-          if (e.key == 'Enter') {
-            gameState = gameState == 'start' ? 'play' : 'start';
-            if (gameState == 'play') {
-              message.innerHTML = 'Partie en cours';
-              message.style.left = 42 + 'vw';
-              requestAnimationFrame(() => {
+let paddle_1 = document.querySelector('.paddle_1');
+let paddle_2 = document.querySelector('.paddle_2');
+let board = document.querySelector('.board');
+let initial_ball = document.querySelector('.ball');
+let ball = document.querySelector('.ball');
+let score_1 = document.querySelector('.player_1_score');
+let score_2 = document.querySelector('.player_2_score');
+let message = document.querySelector('.message');
+let paddle_1_coord = paddle_1.getBoundingClientRect();
+let paddle_2_coord = paddle_2.getBoundingClientRect();
+let initial_ball_coord = ball.getBoundingClientRect();
+let ball_coord = initial_ball_coord;
+let board_coord = board.getBoundingClientRect();
+let paddle_common = document.querySelector('.paddle').getBoundingClientRect();
+let dx = Math.floor(Math.random() * 4) + 3;
+let dy = Math.floor(Math.random() * 4) + 3;
+let dxd = Math.floor(Math.random() * 2);
+let dyd = Math.floor(Math.random() * 2);
+
+document.addEventListener('keydown', (e) => {
+    if (e.key == 'Enter') {
+        gameState = gameState == 'start' ? 'play' : 'start';
+        if (gameState == 'play') {
+            message.innerHTML = 'Partie en cours';
+            message.style.left = 42 + 'vw';
+            requestAnimationFrame(() => {
                 dx = Math.floor(Math.random() * 4) + 3;
                 dy = Math.floor(Math.random() * 4) + 3;
                 dxd = Math.floor(Math.random() * 2);
                 dyd = Math.floor(Math.random() * 2);
                 moveBall(dx, dy, dxd, dyd);
-              });
-            }
-          }
-          if (gameState == 'play') {
-            if (e.key == 'z') {
-              paddle_1.style.top =
-                Math.max(
-                  board_coord.top,
-                  paddle_1_coord.top - window.innerHeight * 0.06
-                ) + 'px';
-              paddle_1_coord = paddle_1.getBoundingClientRect();
-            }
-            if (e.key == 's') {
-              paddle_1.style.top =
-                Math.min(
-                  board_coord.bottom - paddle_common.height,
-                  paddle_1_coord.top + window.innerHeight * 0.06
-                ) + 'px';
-              paddle_1_coord = paddle_1.getBoundingClientRect();
-            }
-        
-            if (e.key == 'ArrowUp') {
-              paddle_2.style.top =
-                Math.max(
-                  board_coord.top,
-                  paddle_2_coord.top - window.innerHeight * 0.1
-                ) + 'px';
-              paddle_2_coord = paddle_2.getBoundingClientRect();
-            }
-            if (e.key == 'ArrowDown') {
-              paddle_2.style.top =
-                Math.min(
-                  board_coord.bottom - paddle_common.height,
-                  paddle_2_coord.top + window.innerHeight * 0.1
-                ) + 'px';
-              paddle_2_coord = paddle_2.getBoundingClientRect();
-            }
-          }
-        });
-        
-        function moveBall(dx, dy, dxd, dyd) {
-          if (ball_coord.top <= board_coord.top) {
-            dyd = 1;
-          }
-          if (ball_coord.bottom >= board_coord.bottom) {
-            dyd = 0;
-          }
-          if (
-            ball_coord.left <= paddle_1_coord.right &&
-            ball_coord.top >= paddle_1_coord.top &&
-            ball_coord.bottom <= paddle_1_coord.bottom
-          ) {
-            dxd = 1;
-            dx = Math.floor(Math.random() * 4) + 3;
-            dy = Math.floor(Math.random() * 4) + 3;
-          }
-          if (
-            ball_coord.right >= paddle_2_coord.left &&
-            ball_coord.top >= paddle_2_coord.top &&
-            ball_coord.bottom <= paddle_2_coord.bottom
-          ) {
-            dxd = 0;
-            dx = Math.floor(Math.random() * 4) + 3;
-            dy = Math.floor(Math.random() * 4) + 3;
-          }
-          if (
-            ball_coord.left <= board_coord.left ||
-            ball_coord.right >= board_coord.right
-          ) {
-            if (ball_coord.left <= board_coord.left) {
-              score_2.innerHTML = +score_2.innerHTML + 1;
-            } else {
-              score_1.innerHTML = +score_1.innerHTML + 1;
-            }
-            gameState = 'start';
-        
-            ball_coord = initial_ball_coord;
-            ball.style = initial_ball.style;
-            return;
-          }
-          ball.style.top = ball_coord.top + dy * (dyd == 0 ? -1 : 1) + 'px';
-          ball.style.left = ball_coord.left + dx * (dxd == 0 ? -1 : 1) + 'px';
-          ball_coord = ball.getBoundingClientRect();
-          requestAnimationFrame(() => {
-            moveBall(dx, dy, dxd, dyd);
-          });
+            });
         }
- // Ajout de la détection mobile
- function isMobile() {
-    return /iPhone|iPad|iPod|Android/i.test(navigator.userAgent) || 
-           /Mobile|Tablet|Android|iOS|iPhone|iPad/i.test(navigator.userAgent);
+    }
+    if (gameState == 'play') {
+        if (e.key == 'z') {
+            paddle_1.style.top = Math.max(board_coord.top, paddle_1_coord.top - window.innerHeight * 0.06) + 'px';
+            paddle_1_coord = paddle_1.getBoundingClientRect();
+        }
+        if (e.key == 's') {
+            paddle_1.style.top = Math.min(board_coord.bottom - paddle_common.height, paddle_1_coord.top + window.innerHeight * 0.06) + 'px';
+            paddle_1_coord = paddle_1.getBoundingClientRect();
+        }
+
+        if (e.key == 'ArrowUp') {
+            paddle_2.style.top = Math.max(board_coord.top, paddle_2_coord.top - window.innerHeight * 0.1) + 'px';
+            paddle_2_coord = paddle_2.getBoundingClientRect();
+        }
+        if (e.key == 'ArrowDown') {
+            paddle_2.style.top = Math.min(board_coord.bottom - paddle_common.height, paddle_2_coord.top + window.innerHeight * 0.1) + 'px';
+            paddle_2_coord = paddle_2.getBoundingClientRect();
+        }
+    }
+});
+
+function moveBall(dx, dy, dxd, dyd) {
+    if (ball_coord.top <= board_coord.top) {
+        dyd = 1;
+    }
+    if (ball_coord.bottom >= board_coord.bottom) {
+        dyd = 0;
+    }
+    if (ball_coord.left <= paddle_1_coord.right && ball_coord.top >= paddle_1_coord.top && ball_coord.bottom <= paddle_1_coord.bottom) {
+        dxd = 1;
+        dx = Math.floor(Math.random() * 4) + 3;
+        dy = Math.floor(Math.random() * 4) + 3;
+    }
+    if (ball_coord.right >= paddle_2_coord.left && ball_coord.top >= paddle_2_coord.top && ball_coord.bottom <= paddle_2_coord.bottom) {
+        dxd = 0;
+        dx = Math.floor(Math.random() * 4) + 3;
+        dy = Math.floor(Math.random() * 4) + 3;
+    }
+    if (ball_coord.left <= board_coord.left || ball_coord.right >= board_coord.right) {
+        if (ball_coord.left <= board_coord.left) {
+            score_2.innerHTML = +score_2.innerHTML + 1;
+        } else {
+            score_1.innerHTML = +score_1.innerHTML + 1;
+        }
+        gameState = 'start';
+        ball_coord = initial_ball_coord;
+        ball.style = initial_ball.style;
+        return;
+    }
+    ball.style.top = ball_coord.top + dy * (dyd == 0 ? -1 : 1) + 'px';
+    ball.style.left = ball_coord.left + dx * (dxd == 0 ? -1 : 1) + 'px';
+    ball_coord = ball.getBoundingClientRect();
+    requestAnimationFrame(() => {
+        moveBall(dx, dy, dxd, dyd);
+    });
+}
+
+function isMobile() {
+    return /iPhone|iPad|iPod|Android/i.test(navigator.userAgent) || /Mobile|Tablet|Android|iOS|iPhone|iPad/i.test(navigator.userAgent);
 }
 
 function isScreenSmall() {
@@ -137,54 +118,48 @@ function updateMessage() {
     }
 }
 
-
 updateMessage();
-
 
 window.addEventListener('resize', () => {
     updateMessage();
 });
 
-
 window.addEventListener('orientationchange', () => {
     updateMessage();
 });
 
+function simulateKeyPress(key) {
+    const event = new KeyboardEvent('keydown', { key: key });
+    document.dispatchEvent(event);
+}
 
-        function simulateKeyPress(key) {
-            const event = new KeyboardEvent('keydown', { key: key });
-            document.dispatchEvent(event);
-        }
+document.querySelector('.player1-up').addEventListener('touchstart', function(e) {
+    e.preventDefault();
+    simulateKeyPress('z');
+});
 
+document.querySelector('.player1-down').addEventListener('touchstart', function(e) {
+    e.preventDefault();
+    simulateKeyPress('s');
+});
 
-        document.querySelector('.player1-up').addEventListener('touchstart', function(e) {
-            e.preventDefault();
-            simulateKeyPress('z');
-        });
+document.querySelector('.player2-up').addEventListener('touchstart', function(e) {
+    e.preventDefault();
+    simulateKeyPress('ArrowUp');
+});
 
-        document.querySelector('.player1-down').addEventListener('touchstart', function(e) {
-            e.preventDefault();
-            simulateKeyPress('s');
-        });
+document.querySelector('.player2-down').addEventListener('touchstart', function(e) {
+    e.preventDefault();
+    simulateKeyPress('ArrowDown');
+});
 
-        document.querySelector('.player2-up').addEventListener('touchstart', function(e) {
-            e.preventDefault();
-            simulateKeyPress('ArrowUp');
-        });
+document.querySelector('.start-button').addEventListener('touchstart', function(e) {
+    e.preventDefault();
+    simulateKeyPress('Enter');
+});
 
-        document.querySelector('.player2-down').addEventListener('touchstart', function(e) {
-            e.preventDefault();
-            simulateKeyPress('ArrowDown');
-        });
-
-        document.querySelector('.start-button').addEventListener('touchstart', function(e) {
-            e.preventDefault();
-            simulateKeyPress('Enter');
-        });
-
-
-        document.querySelectorAll('.control-button, .start-button').forEach(button => {
-            button.addEventListener('touchmove', function(e) {
-                e.preventDefault();
-            });
-        });
+document.querySelectorAll('.control-button, .start-button').forEach(button => {
+    button.addEventListener('touchmove', function(e) {
+        e.preventDefault();
+    });
+});
